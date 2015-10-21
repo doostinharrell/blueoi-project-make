7b63359708d458c557d5fe4d8b846c7236022675
Thomas Dustin Harrell <doostinharrell@gmail.com>
2015-10-21 14:33:32 -0400

    updated build.sh file to symlink files directory and copy settings.php

--- build.sh
+++ build.sh
@@ -8,21 +8,30 @@ TMP_PATH="$PROJECT_PATH/tmp"
 BUILD_PATH="$PROJECT_PATH/www"
 
 echo '=============================================='
-echo '               Syncing Files                  '
+echo '         Building Drupal Installation         '
 echo '=============================================='
 
 set -e
-
-echo '=============================================='
-echo '         Building Drupal Installation         '
-echo '=============================================='
 
 if [ $BUILD_PATH ]; then
   rm -Rf $BUILD_PATH
 fi
 
+if [ $TMP_PATH ]; then
+  rm -Rf $TMP_PATH
+fi
+
 drush make $DRUSH_OPTS "$WORKING_PATH/$MAKEFILE" $TMP_PATH
 rsync -vrq --delete --exclude-from=$WORKING_PATH/rsync.exclude $TMP_PATH/ $BUILD_PATH/
+
+echo '=============================================='
+echo '              Creating Symlinks              '
+echo '=============================================='
+
+# Create symlinks
+if [ ! -L "$BUILD_PATH/sites/default/files" ]; then
+  ln -s ../../../project/files ../www/sites/default/files
+fi
 
 if [ ! -L "$BUILD_PATH/sites/default/libraries" ]; then
   ln -s ../../../project/libraries ../www/sites/default/libraries
@@ -36,17 +45,25 @@ if [ ! -L "$BUILD_PATH/sites/default/themes" ]; then
   ln -s ../../../project/themes ../www/sites/default/themes
 fi
 
-rm -Rf $TMP_PATH
+echo '=============================================='
+echo '             Copying settings.php             '
+echo '=============================================='
 
-set +e
+# Copy settings.php
+if [ ! -L "$BUILD_PATH/sites/default/settings.php" ]; then
+  cp ./settings.php ../www/sites/default/settings.php
+fi
 
 echo '=============================================='
 echo '                   Clean Up                   '
 echo '=============================================='
 
+rm -Rf $TMP_PATH
 cd $BUILD_PATH
 drush rr
 drush cc all
+
+set +e
 
 echo '=============================================='
 echo '            Project Build Complete            '
